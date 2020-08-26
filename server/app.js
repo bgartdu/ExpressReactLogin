e176ce115a49c97f1212433e156b9f7997aa3ef2
Brandon Gart <brandongart@gmail.com>
2020-08-26 17:08:33 -0600

    Adds cors package

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors")
+
+const corsConfig = {
+	origin: "http://localhost:3000",// this should come from a config file
+}
 
 const app = express();
+app.use(cors(corsConfig))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
