99c386d2aa6234a3ea16b209a0faf6b9ea42116e
Brandon Gart <brandongart@gmail.com>
2020-08-26 17:18:09 -0600

    Adds currently logged in user to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,15 +3,22 @@ import Global from "../Global";
 
 
 class Header extends React.Component {
-    static contextType = Global;
-    
-    // constructor() {
-    //     super();
-    // }
-    
-    render() {
-        return <div></div>
-    }    
+	static contextType = Global;
+	
+	// constructor() {
+	//	 super();
+	// }
+	
+	render() {
+		return <div className ="row">
+			<div className="col s8"><h2>Login Example</h2></div>
+			<div className="col s4">
+				{
+					this.context.username ? this.context.username : <a href = "/login"> go to login </a>
+				}
+			</div>
+		</div>
+	}	
 }
 
 export default Header;
