85eebab85a966dfb5ac03c1190e7edfbd0f55209
Brandon Gart <brandongart@gmail.com>
2020-08-27 16:25:55 -0600

    adds password change rout to server

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,6 +5,79 @@ const db = require("../db");
 
 const router = express.Router();
 
+
+//TODO: Get this value from configuration
+const SECRET = "thisIsNotAGoodSecret,ChangeMePlease"
+
+router.post("/changePassword", async function(req, res){
+	const { oldPassword, newPassword, confirmPassword, token} = req.body;
+
+	if (!oldPassword || !newPassword || !confirmPassword || !token) {
+		res.json({
+			success: false,
+			message: "Missing Information"
+		});
+		return;
+	}
+
+	let id = null;
+	let username = null;
+
+	try {
+		const unpacked = jwt.verify(token, SECRET)
+		id = unpacked.id
+		username = unpacked.username;
+	} catch (err) {
+		res.json({
+			success: false,
+			message: "failed to unpack token",
+			err,
+		})
+		return;
+	}
+
+	const check = await db.User.findOne( { id });
+	if (!check) {
+		res.json({
+			success: false,
+			message: "user no longer exists"
+		});
+		return;
+	}
+
+	const hash = check.hash
+	if (!await argon2.verify(hash, oldPassword)) {
+		res.json({
+			success: false,
+			message: "Old password does not match current password"
+		});
+		return;
+	}
+
+	if (newPassword !== confirmPassword) {
+		res.json({
+			success: false,
+			message: "new passwords do not match"
+		});
+		return;
+	}
+
+	check.hash = await argon2.hash(newPassword);
+
+	try {
+		await check.save();
+	} catch (err) {
+		res.json({
+			success: false,
+			message: "Database error",
+			err
+		});
+		return;
+	}
+
+	res.json({ success: true })
+})
+
 router.post("/login", async function(req, res) {
 	const { username, password } = req.body;
 	
@@ -35,7 +108,7 @@ router.post("/login", async function(req, res) {
 
 	const token = jwt.sign(
 		{ id: check.id, username: check.username },
-		"thisIsNotAGoodSecret,ChangeMePlease", 
+		SECRET, 
 		{ expiresIn: 60 * 60 }
 	);
 
